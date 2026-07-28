1c6f3fd67c24f6009f8e5596db1ac77b63582714
Lemon <sdh22553@konkuk.ac.kr>
2026-07-28 23:25:16 +0900

    회원가입과 로그인 요청 매핑 추가

---
+++ src/main/java/com/lemonade0209/minibank/web/MemberController.java
@@ -0,0 +1,30 @@
+package com.lemonade0209.minibank.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/members/add")
+    public String addForm() {
+        log.info("GET /members/add handler called");
+        return "members/add";
+    }
+
+    @PostMapping("/members/add")
+    public String addMember() {
+        log.info("POST /members/add handler called");
+        return "members/add";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        log.info("GET /login handler called");
+        return "login";
+    }
+}
