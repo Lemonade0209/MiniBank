70eb9b31b52b6a4cfe33407529e2b83ef8b55a94
Lemon <sdh22553@konkuk.ac.kr>
2026-08-07 23:54:19 +0900

    타임리프 반복 조건과 공통 조각 적용

--- src/main/java/com/lemonade0209/minibank/web/HomeController.java
+++ src/main/java/com/lemonade0209/minibank/web/HomeController.java
@@ -1,12 +1,14 @@
 package com.lemonade0209.minibank.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("projectStatus", "애플리케이션 정상 작동 중");
         return "home";
     }
 }
